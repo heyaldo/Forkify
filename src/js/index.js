cc223c60548f508ff37929dc273f884921bda5e7
developer-goplek <developer@goplek.com>
2020-03-29 17:49:27 -0600

    making first api calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 // Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = res.data.recipes[2];
+        console.log(recipes);
+    } catch(error) {
+        alert(error)
+    }
+        
+}
+getResults('pizza');
