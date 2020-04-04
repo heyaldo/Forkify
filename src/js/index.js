26a9d17a5b73faa3bd41af2efe0fa3e4f83e5ba0
developer-goplek <developer@goplek.com>
2020-04-03 20:56:46 -0600

    Search View 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 // Global app controller
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
 
 /** Global state of the app
  * - Search object
@@ -11,23 +14,27 @@ const state = {};
 
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = 'pizza';
+    const query = searchView.getInput();
+    console.log(query);
     
     if(query) {
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearList();
 
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render the results on UI
         console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+// An object with all elements that we need from the DOM
+
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchList: document.querySelector('.results__list')
+}
